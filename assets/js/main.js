a314092b53288b645be11e4093fb358a27be6aa6
ctripoll <ctripoll@ncsu.edu>
2018-07-19 23:42:22 -0400

    working on overall aesthetic

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).on("scroll", function() {
+    if($(document).scrollTop() > 725) {
+        $("nav").addClass("shrink");
+    } else {
+        $("nav").removeClass("shrink");
+    }
+});
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+        }
+    });
+});
